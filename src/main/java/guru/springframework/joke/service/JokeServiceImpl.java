e36f99e28ef70787710730f7553601e7e23e0326
alivankoff <al.ivankoff@gmail.com>
2018-04-09 12:52:07 -0400

    Step#3 created service interface /service implementaiton

--- src/main/java/guru/springframework/joke/service/JokeServiceImpl.java
+++ src/main/java/guru/springframework/joke/service/JokeServiceImpl.java
@@ -1,16 +1,24 @@
 package guru.springframework.joke.service;
 
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
+
+
 
 
 @Service
 public class JokeServiceImpl implements JokeService {
 
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
 
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
 
     @Override
     public String getJoke() {
-        return null;
+        return chuckNorrisQuotes.getRandomQuote();
     }
+
 }
